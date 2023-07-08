d4db5c30d0669721dc7f6aa04b2daaeb5c62e090
Hao Doan Vo Nhut <20110641@student.hcmute.edu.vn>
2023-07-08 15:00:58 +0700

    Initial commit

---
+++ src/main/java/com/vocahype/VocahypeApplication.java
@@ -0,0 +1,13 @@
+package com.vocahype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocahypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocahypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vocahype/VocahypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocahype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocahypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
