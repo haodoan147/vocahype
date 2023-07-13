2ab402f00f8c64dbbfb10a65d4e1cb07dbaad2ac
Hao Doan Vo Nhut <20110641@student.hcmute.edu.vn>
2023-07-13 01:46:13 +0000

    Add staging

--- src/main/java/com/vocahype/service/WordService.java
+++ src/main/java/com/vocahype/service/WordService.java
@@ -21,21 +21,22 @@ import static com.vocahype.util.Constants.WORD_COUNT;
 @RequiredArgsConstructor
 public class WordService {
 
+    public static final int BIAS = 1000;
     private final WordRepository wordRepository;
 
     public List<Word> getWordList() {
         List<Word> wordList = new ArrayList<>();
         LongRange[] ranges = {
-                new LongRange(0L, WORD_COUNT / 10, 10),
-                new LongRange(WORD_COUNT / 10, WORD_COUNT / 10 * 2, 9),
-                new LongRange(WORD_COUNT / 10 * 2, WORD_COUNT / 10 * 3, 7),
-                new LongRange(WORD_COUNT / 10 * 3, WORD_COUNT / 10 * 4, 6),
-                new LongRange(WORD_COUNT / 10 * 4, WORD_COUNT / 10 * 5, 5),
-                new LongRange(WORD_COUNT / 10 * 5, WORD_COUNT / 10 * 6, 4),
-                new LongRange(WORD_COUNT / 10 * 6, WORD_COUNT / 10 * 7, 3),
-                new LongRange(WORD_COUNT / 10 * 7, WORD_COUNT / 10 * 8, 3),
-                new LongRange(WORD_COUNT / 10 * 8, WORD_COUNT / 10 * 9, 2),
-                new LongRange(WORD_COUNT / 10 * 9, WORD_COUNT, 1)
+                new LongRange(0L, WORD_COUNT / BIAS, 10),
+                new LongRange(WORD_COUNT / BIAS, WORD_COUNT / BIAS * 2, 9),
+                new LongRange(WORD_COUNT / BIAS * 2, WORD_COUNT / BIAS * 3, 7),
+                new LongRange(WORD_COUNT / BIAS * 3, WORD_COUNT / BIAS * 4, 6),
+                new LongRange(WORD_COUNT / BIAS * 4, WORD_COUNT / BIAS * 5, 5),
+                new LongRange(WORD_COUNT / BIAS * 5, WORD_COUNT / BIAS * 6, 4),
+                new LongRange(WORD_COUNT / BIAS * 6, WORD_COUNT / BIAS * 7, 3),
+                new LongRange(WORD_COUNT / BIAS * 7, WORD_COUNT / BIAS * 8, 3),
+                new LongRange(WORD_COUNT / BIAS * 8, WORD_COUNT / BIAS * 9, 2),
+                new LongRange(WORD_COUNT / BIAS * 9, WORD_COUNT / BIAS, 1)
         };
         for (LongRange range : ranges) {
             int numRecordsToFetch = range.getNumRecords();
