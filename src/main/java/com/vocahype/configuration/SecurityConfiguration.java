23887e7305494e99f039206e5013c4e0de7aa83c
Hao Doan Vo Nhut <20110641@student.hcmute.edu.vn>
2023-08-08 03:10:21 +0700

    CORS configuration

--- src/main/java/com/vocahype/configuration/SecurityConfiguration.java
+++ src/main/java/com/vocahype/configuration/SecurityConfiguration.java
@@ -15,8 +15,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +31,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeRequests().anyRequest().permitAll()
                 .and()
                 .sessionManagement()
@@ -67,5 +71,16 @@ public class SecurityConfiguration {
         };
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token", "Referer", "Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods", "Access-Control-Allow-Credentials", "Access-Control-Expose-Headers", "Access-Control-Max-Age", "Access-Control-Request-Headers", "Access-Control-Request-Method", "Age", "Allow", "Alternates", "Content-Range", "Content-Disposition", "Content-Description", "Content-Encoding", "Content-Language", "Content-Length", "Content-Location", "Content-MD5", "Content-Transfer-Encoding", "Content-Type", "Date", "Last-Modified", "Link", "Location", "MIME-Version", "Retry-After", "Server", "Transfer-Encoding", "Vary", "WWW-Authenticate", "X-Frame-Options", "X-Powered-By", "X-Requested-With", "X-Content-Type-Options", "X-Forwarded-Proto", "X-XSS-Protection", "X-Application-Context", "X-Cache", "X-Cache-Hits", "X-Cache-Expires", "X-Cache-Key", "X-Cache-Remote", "X-Cache-Debug", "X-Request-ID", "X-Correlation-ID", "X-Real-IP", "X-Forwarded-For", "X-Forwarded-Host", "X-Forwarded-Server", "X-Forwarded-Port", "X-Forwarded-Prefix", "X-Forwarded-Ssl", "X-Forwarded-Protocol", "X-Forwarded-Https", "X-Forwarded-Ip", "X-ProxyUser-Ip", "X-ProxyUser-Realm", "X-ProxyUser-Groups", "X-ProxyUser-Subject", "X-ProxyUser-Session", "X-ProxyUser-AccessToken", "X-ProxyUser-IdToken", "X-ProxyUser-RefreshToken", "X-ProxyUser-Token-Expires-In", "X-ProxyUser-Token-Expires-At", "X-ProxyUser-Session-Expires-In", "X-ProxyUser-Session-Expires-At", "X-ProxyUser-Realm-Access-Token", "X-ProxyUser-Realm-Access-Refresh-Token", "X-ProxyUser-Realm-Access-Token-Expires-In", "X-ProxyUser-Realm-Access-Token-Expires-At", "X-ProxyUser-Realm-Access-Refresh-Token-Expires-In", "X-ProxyUser-Realm-Access-Refresh-Token-Expires-At", "X-Custom-Header"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
