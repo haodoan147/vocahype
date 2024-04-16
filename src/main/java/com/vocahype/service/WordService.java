508a9c23e3ecc6fd2325a3418b4658e8c1016569
Hao Doan Vo Nhut <20110641@student.hcmute.edu.vn>
2024-04-17 04:29:10 +0700

    complete update word

--- src/main/java/com/vocahype/service/WordService.java
+++ src/main/java/com/vocahype/service/WordService.java
@@ -2,12 +2,15 @@ package com.vocahype.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vocahype.dto.DefinitionDTO;
 import com.vocahype.dto.SynonymDTO;
 import com.vocahype.dto.WordDTO;
 import com.vocahype.dto.enumeration.WordStatus;
 import com.vocahype.entity.Word;
 import com.vocahype.entity.Meaning;
 import com.vocahype.exception.InvalidException;
+import com.vocahype.repository.MeaningRepository;
+import com.vocahype.repository.PosRepository;
 import com.vocahype.repository.SynonymRepository;
 import com.vocahype.repository.WordRepository;
 import com.vocahype.util.SecurityUtil;
@@ -29,6 +32,8 @@ public class WordService {
     private final ModelMapper modelMapper;
     private final SynonymRepository synonymRepository;
     private final ObjectMapper objectMapper;
+    private final MeaningRepository meaningRepository;
+    private final PosRepository posRepository;
 
     public WordDTO getWordById(Long id) {
         WordDTO word = wordRepository.findWordDTOById(id, SecurityUtil.getCurrentUserId()).orElseThrow(() -> new InvalidException("Word not found", "Not found any word with id: " + id));
@@ -88,16 +93,35 @@ public class WordService {
         if (resourceWord.getPhoneticEnd() != null && !resourceWord.getPhoneticEnd().equals(targetWord.getPhoneticEnd())) {
             targetWord.setPhoneticEnd(resourceWord.getPhoneticEnd());
         }
-        if (jsonNode.get("data").get(0).get("relationships").has("meanings")
-                && jsonNode.get("data").get(0).get("relationships").get("meanings").get("data").size() != targetWord.getMeanings().size()) {
-            Set<Meaning> meanings = new HashSet<>();
-            jsonNode.get("data").get(0).get("relationships").get("meanings").get("data").forEach(meaning -> {
-                Long meaningId = meaning.get("id").asLong();
-                meanings.add(Meaning.builder().id(meaningId).build());
-            });
-            targetWord.setMeanings(meanings);
-        }
+        Set<Meaning> meanings = new HashSet<>();
+        jsonNode.get("included").forEach(included -> {
+            if (included.get("type").asText().equals("meaning")) {
+                Long meaningId = included.get("id").asLong();
+                Meaning meaning = meaningRepository.findById(meaningId).orElseThrow(() ->
+                        new InvalidException("Meaning not found", "Not found any meaning with id: " + meaningId));
+                if (!included.get("relationships").has("pos")) {
+                    throw new InvalidException("Pos not found", "Pos not found in meaning with id: " + meaningId);
+                }
+                JsonNode dataPos = included.get("relationships").get("pos").get("data");
+                if (dataPos.has("id") && !dataPos.get("id").asText().equals(meaning.getPos().getPosTag())) {
+                    meaning.setPos(posRepository.findById(dataPos.get("id")
+                            .asText()).orElseThrow(() -> new InvalidException("Pos not found",
+                            "Not found any pos with id: "
+                            + dataPos.get("id").asText())));
+                }
+                Set<DefinitionDTO> definitionDTOs = new HashSet<>();
+                if (included.get("attributes").has("definitions")) {
+                    included.get("attributes").get("definitions").forEach(definition -> {
+                        DefinitionDTO definitionDTO = objectMapper.convertValue(definition, DefinitionDTO.class);
+                        definitionDTOs.add(definitionDTO);
+                    });
+                }
+                meaning.setDefinitions(definitionDTOs);
+                meanings.add(meaning);
+            }
+        });
+        targetWord.setMeanings(meanings);
 
-        return new WordDTO(targetWord);
+        return new WordDTO(targetWord, true, true);
     }
 }
