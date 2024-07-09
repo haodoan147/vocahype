42620106bdafb7db2124e3ab014c89d58d9511f0
Hao Doan Vo Nhut <20110641@student.hcmute.edu.vn>
2024-07-10 01:51:45 +0700

    Update word topic and topic

--- src/main/java/com/vocahype/repository/WordTopicRepository.java
+++ src/main/java/com/vocahype/repository/WordTopicRepository.java
@@ -4,8 +4,10 @@ import com.vocahype.entity.WordTopic;
 import com.vocahype.entity.WordTopicID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Set;
 
 public interface WordTopicRepository extends JpaRepository<WordTopic, WordTopicID> {
     void deleteAllByTopicIdAndWordIdNotIn(final Long id, final Set<Long> collect);
+    List<WordTopic> findAllByTopic_Id(final Long id);
 }
